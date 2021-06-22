94753c9802e5b5fc658f5eba8be2e0754db9b9e6
TheWallsHaveEyes <77647510+TheWallsHaveEyes@users.noreply.github.com>
2021-06-22 13:50:39 -0400

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,37 @@
+class Paper{
+ constructor(x,y,r){
+     var options={
+         stStatic:false,
+         restitution:0.3,
+         friction:0,
+         density:1.2
+
+     }
+     this.x=x;
+		this.y=y;
+        this.image=loadImage("paper.png");
+		this.r=r;
+		this.body=Bodies.circle(x, y, r, options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var Pos=this.body.position;		
+
+            var angle=this.body.angle;
+
+			push()
+			translate(Pos.x, Pos.y);
+            rotate(angle);
+			imageMode(CENTER)
+			//strokeWeight(4);
+			//fill(128,128,128)
+			image(this.image,0,0,this.r, this.r);
+			pop()
+
+
+
+ }
+}
